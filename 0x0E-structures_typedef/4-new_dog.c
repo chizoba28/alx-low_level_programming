400b5fe0cffa5b617016d707ac54e592ca0d52c6
chizoba28 <chizobaanyaeji65@gmail.com>
2023-05-22 13:39:27 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,25 +1,4 @@
-[8:25 pm, 22/05/2023] +234 812 220 4393: #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
-
-/**
-* init_dog - a function that initialize a variable of type struct dog
-* @d: a pointer to the struct dog
-* @name: a string 'name'
-* @age: a float 'age'
-* @owner: a string 'owner'
-* Return: returns nothing
-*/
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
-[8:26 pm, 22/05/2023] Purple Gleam collection: This pls
-[8:33 pm, 22/05/2023] +234 812 220 4393: #include <stdlib.h>
 #include "dog.h"
 
 /**
